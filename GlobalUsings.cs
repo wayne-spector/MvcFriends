f84da2a86df30146b0d286021cc7b31e91467bfa
wayne-spector <sadboii1997@gmail.com>
2026-02-18 05:23:49 +0800

    Revert "Project Initial"
    This reverts commit cffef565906537d7db6a2e441eddebd300de194e.

--- GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-global using Microsoft.EntityFrameworkCore;
-global using System.ComponentModel.DataAnnotations;
-global using System.ComponentModel.DataAnnotations.Schema;
-global using MvcFriends.Models;
